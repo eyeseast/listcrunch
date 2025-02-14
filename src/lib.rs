9394ebb2f267132cd110c5dca3849a91dfd35c8c
Chris Amico <eyeseast@gmail.com>
2025-02-14 16:41:04 -0500

    uncrunch and passing all original tests

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,69 @@ pub fn crunch<T: Hash + Eq + Display>(list: &[T]) -> String {
     parts.join(";")
 }
 
-pub fn uncrunch() {}
+pub fn uncrunch(s: &str) -> Result<Vec<&str>, &'static str> {
+    // for an empty string, return an empty vector
+    if s.trim().len() == 0 {
+        return Ok(Vec::new());
+    }
+
+    let mut results: Vec<(u32, &str)> = Vec::new();
+    let parts = s.split(";");
+    for part in parts {
+        let mut subparts = part.split(":");
+
+        // value and specs
+        let value = match subparts.next() {
+            Some(s) => s,
+            None => return Err("Each ';'-delimited region must have exactly one ':'"),
+        };
+        let specs = match subparts.next() {
+            Some(s) => s,
+            None => return Err("Each ';'-delimited region must have exactly one ':'"),
+        };
+
+        // anything left is an error
+        if subparts.count() > 0 {
+            return Err("Each ';'-delimited region must have exactly one ':'");
+        }
+
+        for spec in specs.split(",") {
+            if spec.contains("-") {
+                // it's a range
+                let mut range = spec.split("-");
+                let start = match range.next() {
+                    Some(s) => s,
+                    None => return Err("Each page range (e.g. 3-5) must have exactly one '-'"),
+                };
+
+                let end = match range.next() {
+                    Some(s) => s,
+                    None => return Err("Each page range (e.g. 3-5) must have exactly one '-'"),
+                };
+
+                // convert to numbers, todo: error handling
+                let start = start.parse::<i32>().unwrap();
+                let end = end.parse::<i32>().unwrap();
+
+                for i in start..end + 1 {
+                    results.push((i as u32, value));
+                }
+            } else {
+                // just one page
+                let spec: u32 = match spec.parse() {
+                    Ok(i) => i,
+                    Err(_) => return Err("Couldn't parse range"),
+                };
+
+                results.push((spec, value));
+            }
+        }
+    }
+
+    // sort and return
+    results.sort_by(|a, b| a.0.cmp(&b.0));
+    Ok(results.into_iter().map(|(_, value)| value).collect())
+}
 
 fn build_index<T: Hash + Eq>(list: &[T]) -> Vec<(&T, Vec<i32>)> {
     let mut index: HashMap<&T, Vec<i32>> = HashMap::new();
@@ -69,6 +131,7 @@ fn end_run(run: (i32, i32)) -> String {
 mod tests {
     use super::*;
 
+    // crunch
     #[test]
     fn basic_crunch() {
         assert_eq!(crunch(&vec![1, 2, 3]), "1:0;2:1;3:2")
@@ -106,5 +169,44 @@ mod tests {
     #[test]
     fn single_item_crunch() {
         assert_eq!(crunch(&vec![77]), "77:0")
+    }
+
+    // uncrunch
+    #[test]
+    fn basic_uncrunch() {
+        assert_eq!(uncrunch("1:0;2:1;3:2"), Ok(vec!["1", "2", "3"]))
+    }
+
+    #[test]
+    fn basic_run_uncrunch() {
+        assert_eq!(uncrunch("1:0;2:1;3:2"), Ok(vec!["1", "2", "3"]))
+    }
+
+    #[test]
+    fn basic_broken_run_uncrunch() {
+        assert_eq!(uncrunch("1:0,2;2:1"), Ok(vec!["1", "2", "1"]))
+    }
+
+    #[test]
+    fn order_uncrunch() {
+        assert_eq!(uncrunch("2:0;1:1"), Ok(vec!["2", "1"]))
+    }
+
+    #[test]
+    fn advanced_uncrunch() {
+        assert_eq!(
+            uncrunch("50:0-1,3-4;3:2,5;60:6;70:7-8"),
+            Ok(vec!["50", "50", "3", "50", "50", "3", "60", "70", "70"])
+        )
+    }
+
+    #[test]
+    fn empty_uncrunch() {
+        assert_eq!(uncrunch(""), Ok(vec![]))
+    }
+
+    #[test]
+    fn single_item_uncrunch() {
+        assert_eq!(uncrunch("77:0"), Ok(vec!["77"]))
     }
 }
